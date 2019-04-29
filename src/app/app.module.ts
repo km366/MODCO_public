eb223479396ea2221657d0fef7eb6aa1e15a3ad0
peter-shoes <bcm73@drexel.edu>
2019-04-29 11:37:28 -0400

    firebase init nonsense for console purposes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,30 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+// Import the AF2 Module--Bruce
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+// AF2 Settings-- Bruce
+export const firebaseConfig = {
+  apiKey: "AIzaSyBUDtCeGfmrguwt52wCzRstK0rUUqSC1y8",
+  authDomain: "modco-3b288.firebaseapp.com",
+  databaseURL: "https://modco-3b288.firebaseio.com",
+  storageBucket: "modco-3b288.appspot.com",
+  messagingSenderId: "1079852918306",
+};
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule
   ],
   providers: [
     StatusBar,
